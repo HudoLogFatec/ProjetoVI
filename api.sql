c7155b6f76eded5df631310381e4829074d0525b
HudoLogFatec <164099908+HudoLogFatec@users.noreply.github.com>
2024-05-11 21:38:06 -0300

    Add files via upload

--- api.sql
+++ api.sql
@@ -1,3 +1,44 @@
+create database API;
+
+use api;
+
+  -- Cria a tabela Analises
+CREATE TABLE Analises AS
+SELECT
+  rotas.ï»¿Emissao AS Emissao,
+  rotas.Entrega,
+  rotas.MÃªs AS Mes,
+  rotas.Ano,
+  rotas.CodFabrica,
+  rotas.CodCliente,
+  rotas.incoterm,
+  rotas.veiculo,
+  rotas.Qtdpallets,
+  rotas.QtdTransp,
+  rotas.VlrFrete,
+  rotas.Dist,
+  fabricas.MUN AS MunFabrica,
+  fabricas.UF AS UFFabrica,
+  clientes.MUN AS MunCliente,
+  CASE
+    WHEN rotas.veiculo = 'P24' THEN 3600
+    WHEN rotas.veiculo = 'P12' THEN 1800
+    ELSE NULL
+  END AS Capacidade,
+  ROUND((rotas.QtdTransp / 
+    CASE
+      WHEN rotas.veiculo = 'P24' THEN 3600
+      WHEN rotas.veiculo = 'P12' THEN 1800
+      ELSE 1 -- Evita divisão por zero
+    END
+  ) * 100, 2) AS Produtividade
+FROM
+  rotas
+JOIN
+  fabricas ON rotas.CodFabrica = fabricas.CodFabrica
+JOIN
+  clientes ON rotas.CodCliente = clientes.CodCliente;
+
 use api;
 
 SELECT * FROM api.analises;
@@ -20,3 +61,5 @@ where Incoterm = 'FOB';
 
 select round(sum(QtdTransp), 2) as	Custo_Total
 from analises;
+  
+ 
